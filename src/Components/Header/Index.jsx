66500f2a1a4a4fd08314c0b1a27ccf2a6470e512
BlackPearlsDev <proeslros2@gmail.com>
2022-08-18 15:55:58 +0200

    4eme commit

--- src/Components/Header/Index.jsx
+++ src/Components/Header/Index.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Header() {
     return (
         <header>
-            {/* <img src="../public/assets/bg.png" alt="Le logo du site" /> */}
+            {/* <img src="../public/assets/logo.png" alt="Le logo du site" /> */}
             <h1>HYPER SERVICES</h1>
             <nav>
                 <a href="#">Services</a>
